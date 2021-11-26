a6f0e3578177e2249b0dd818849bd2753ccd4df1
jerrywusa <mrjerryowu@gmail.com>
2021-11-25 18:36:03 -0800

    boids points toward cursor

--- src/components/Boid.js
+++ src/components/Boid.js
@@ -1,55 +1,93 @@
 import "./Boid.css";
-import { useState } from "react";
+import React from "react";
 import boidArrowhead from "../images/boidArrowhead.png";
 import { NodeGroup } from "react-move";
 import range from "lodash.range";
 import { uid } from "uid";
 
-export default function Boid(props) {
-  const [location, setLocation] = useState([0, 0]);
+class Boid extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      x: Math.random() * 1300,
+      y: Math.random() * 700,
+      deg: Math.random() * 360,
+    };
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+  }
 
-  return (
-    <>
-      <NodeGroup
-        data={range(1).map((d) => {
-          return {
-            key: uid(),
-            x: location[0],
-            y: location[1],
-          };
-        })}
-        keyAccessor={(d) => d.key}
-        start={(data) => {
-          return {
-            x: data.x,
-            y: data.y,
-          };
-        }}
-        update={(data) => {
-          return {
-            x: [data.x],
-            y: [data.y],
-          };
-        }}
-      >
-        {(nodes) => (
-          <>
-            {nodes.map(({ key, data, state: { x, y } }) => (
-              <img
-                className="boid"
-                src={boidArrowhead}
-                alt="boid arrowhead"
-                key={key}
-                style={{
-                  transform: `scale(0.1) translate(${x - 3800}px, ${
-                    y - 4000
-                  }px)`,
-                }}
-              />
-            ))}
-          </>
-        )}
-      </NodeGroup>
-    </>
-  );
+  componentDidMount() {
+    window.addEventListener("mousemove", this.handleMouseMove);
+  }
+
+  handleMouseMove({ pageX: x, pageY: y }) {
+    const centerX = this.state.x + 40;
+    const centerY = this.state.y + 40;
+    const changeX = x - centerX;
+    const changeY = y - centerY;
+    const deg =
+      Math.atan(Math.abs(changeY) / Math.abs(changeX)) * (180 / Math.PI);
+    let finalDeg = 0;
+    if (changeX >= 0 && changeY >= 0) {
+      finalDeg = deg + 90;
+    } else if (changeX < 0 && changeY >= 0) {
+      finalDeg = 270 - deg;
+    } else if (changeX < 0 && changeY < 0) {
+      finalDeg = 270 + deg;
+    } else if (changeX >= 0 && changeY < 0) {
+      finalDeg = 90 - deg;
+    }
+    this.setState({ deg: finalDeg });
+  }
+
+  render() {
+    const numberOfBoidsPerGroup = 1;
+    return (
+      <>
+        <NodeGroup
+          data={range(numberOfBoidsPerGroup).map((d) => {
+            return {
+              key: uid(),
+              x: this.state.x,
+              y: this.state.y,
+              deg: this.state.deg,
+            };
+          })}
+          keyAccessor={(d) => d.key}
+          start={(data) => {
+            return {
+              x: data.x,
+              y: data.y,
+              deg: data.deg,
+            };
+          }}
+          update={(data) => {
+            return {
+              x: [data.x],
+              y: [data.y],
+              deg: [data.deg],
+            };
+          }}
+        >
+          {(nodes) => (
+            <>
+              {nodes.map(({ key, data, state: { x, y, deg } }) => (
+                <img
+                  className="boid"
+                  src={boidArrowhead}
+                  alt="boid arrowhead"
+                  key={key}
+                  style={{
+                    transform: `translate(${x}px, ${y}px) rotate(${deg}deg)`,
+                  }}
+                />
+              ))}
+            </>
+          )}
+        </NodeGroup>
+      </>
+    );
+  }
 }
+
+export default Boid;
--- src/components/Boids.js
+++ src/components/Boids.js
@@ -1,13 +1,14 @@
 import "./Boids.css";
 import Boid from "./Boid";
 import range from "lodash.range";
+import { uid } from "uid";
 
 export default function Boids(props) {
   const numberOfBoids = 1;
   return (
     <>
       {range(numberOfBoids).map((d) => (
-        <Boid />
+        <Boid key={uid()} />
       ))}
     </>
   );
