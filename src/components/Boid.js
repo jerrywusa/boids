db426b2dfda3f80549be9c159dc595247b75fbda
jerrywusa <mrjerryowu@gmail.com>
2021-11-30 00:04:52 -0800

    added Boid info to Boids, turned Boid into functional component. Added eventListeners to Boids

--- src/components/Boid.js
+++ src/components/Boid.js
@@ -3,110 +3,51 @@ import React from "react";
 import boidArrowhead from "../images/blackArrowhead.png";
 import { NodeGroup } from "react-move";
 import range from "lodash.range";
-import { uid } from "uid";
 
-class Boid extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      x: Math.random() * 5300,
-      y: Math.random() * 2700,
-      deg: Math.random() * 360,
-    };
-    this.handleMouseMove = this.handleMouseMove.bind(this);
-  }
-
-  componentDidMount() {
-    window.addEventListener("mousemove", this.handleMouseMove);
-  }
-
-  handleMouseMove({ pageX: x, pageY: y }) {
-    const centerX = this.state.x + 40;
-    const centerY = this.state.y + 40;
-    const changeX = x - centerX;
-    const changeY = y - centerY;
-    const deg =
-      Math.atan(Math.abs(changeY) / Math.abs(changeX)) * (180 / Math.PI);
-    let finalDeg = 0;
-    if (changeX >= 0 && changeY >= 0) {
-      finalDeg = deg + 90;
-    } else if (changeX < 0 && changeY >= 0) {
-      finalDeg = 270 - deg;
-    } else if (changeX < 0 && changeY < 0) {
-      finalDeg = 270 + deg;
-    } else if (changeX >= 0 && changeY < 0) {
-      finalDeg = 90 - deg;
-    }
-    this.setState({ deg: finalDeg });
-
-    const rad = deg * (Math.PI / 180);
-    let newX = this.state.x;
-    let newY = this.state.y;
-    const speed = 30;
-    if (changeX >= 0 && changeY >= 0) {
-      newX += Math.cos(rad) * speed;
-      newY += Math.sin(rad) * speed;
-    } else if (changeX < 0 && changeY >= 0) {
-      newX -= Math.cos(rad) * speed;
-      newY += Math.sin(rad) * speed;
-    } else if (changeX < 0 && changeY < 0) {
-      newX -= Math.cos(rad) * speed;
-      newY -= Math.sin(rad) * speed;
-    } else if (changeX >= 0 && changeY < 0) {
-      newX += Math.cos(rad) * speed;
-      newY -= Math.sin(rad) * speed;
-    }
-    this.setState({ x: newX, y: newY });
-  }
-
-  render() {
-    const numberOfBoidsPerGroup = 1;
-    return (
-      <>
-        <NodeGroup
-          data={range(numberOfBoidsPerGroup).map((d) => {
-            return {
-              key: uid(),
-              x: this.state.x,
-              y: this.state.y,
-              deg: this.state.deg,
-            };
-          })}
-          keyAccessor={(d) => d.key}
-          start={(data) => {
-            return {
-              x: data.x,
-              y: data.y,
-              deg: data.deg,
-            };
-          }}
-          update={(data) => {
-            return {
-              x: [data.x],
-              y: [data.y],
-              deg: [data.deg],
-            };
-          }}
-        >
-          {(nodes) => (
-            <>
-              {nodes.map(({ key, data, state: { x, y, deg } }) => (
-                <img
-                  className="boid"
-                  src={boidArrowhead}
-                  alt="boid arrowhead"
-                  key={key}
-                  style={{
-                    transform: `translate(${x}px, ${y}px) rotate(${deg}deg)`,
-                  }}
-                />
-              ))}
-            </>
-          )}
-        </NodeGroup>
-      </>
-    );
-  }
+export default function Boids({ id, x, y, deg }) {
+  return (
+    <>
+      <NodeGroup
+        data={range(1).map((d) => {
+          return {
+            key: id,
+            x: x,
+            y: y,
+            deg: deg,
+          };
+        })}
+        keyAccessor={(d) => d.key}
+        start={(data) => {
+          return {
+            x: data.x,
+            y: data.y,
+            deg: data.deg,
+          };
+        }}
+        update={(data) => {
+          return {
+            x: [data.x],
+            y: [data.y],
+            deg: [data.deg],
+          };
+        }}
+      >
+        {(nodes) => (
+          <>
+            {nodes.map(({ key, data, state: { x, y, deg } }) => (
+              <img
+                className="boid"
+                src={boidArrowhead}
+                alt="boid arrowhead"
+                key={key}
+                style={{
+                  transform: `translate(${x}px, ${y}px) rotate(${deg}deg)`,
+                }}
+              />
+            ))}
+          </>
+        )}
+      </NodeGroup>
+    </>
+  );
 }
-
-export default Boid;
--- src/components/Boids.js
+++ src/components/Boids.js
@@ -1,15 +1,122 @@
 import "./Boids.css";
 import Boid from "./Boid";
-import range from "lodash.range";
 import { uid } from "uid";
+import React from "react";
 
-export default function Boids(props) {
-  const numberOfBoids = 50;
-  return (
-    <>
-      {range(numberOfBoids).map((d) => (
-        <Boid key={uid()} />
-      ))}
-    </>
-  );
+class Boids extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      boids: [],
+    };
+    this.handleMouseClick = this.handleMouseClick.bind(this);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.handleMouseDown = this.handleMouseDown.bind(this);
+    this.handleMouseUp = this.handleMouseUp.bind(this);
+    let mouseIsDown = false;
+  }
+
+  componentDidMount() {
+    window.addEventListener("click", this.handleMouseClick);
+    window.addEventListener("mousemove", this.handleMouseMove);
+    window.addEventListener("mousedown", this.handleMouseDown);
+    window.addEventListener("mouseup", this.handleMouseUp);
+  }
+
+  handleMouseClick({ pageX: x, pageY: y }) {
+    this.addBoid(x - 40, y - 40);
+  }
+
+  handleMouseMove({ pageX: x, pageY: y }) {
+    if (!this.mouseIsDown) {
+      return;
+    }
+
+    let boids = [...this.state.boids];
+    boids.map((boid) => {
+      const centerX = boid.x + 40;
+      const centerY = boid.y + 40;
+      const changeX = x - centerX;
+      const changeY = y - centerY;
+
+      // (changeX, changeY)
+      //
+      //           *
+      //   (-, -)  *  (+, -)
+      //           *
+      // **********************
+      //           *
+      //   (-, +)  *  (+, +)
+      //           *
+
+      const deg =
+        Math.atan(Math.abs(changeY) / Math.abs(changeX)) * (180 / Math.PI);
+      let finalDeg;
+      console.log(changeX + " " + changeY);
+      if (changeX >= 0 && changeY >= 0) {
+        finalDeg = deg + 90;
+      } else if (changeX <= 0 && changeY >= 0) {
+        finalDeg = 270 - deg;
+      } else if (changeX <= 0 && changeY <= 0) {
+        finalDeg = 270 + deg;
+      } else if (changeX >= 0 && changeY <= 0) {
+        finalDeg = 90 - deg;
+      }
+      boid.deg = finalDeg;
+
+      const rad = deg * (Math.PI / 180);
+      let newX = boid.x;
+      let newY = boid.y;
+      const speed = 20;
+      if (changeX >= 0 && changeY >= 0) {
+        newX += Math.cos(rad) * speed;
+        newY += Math.sin(rad) * speed;
+      } else if (changeX < 0 && changeY >= 0) {
+        newX -= Math.cos(rad) * speed;
+        newY += Math.sin(rad) * speed;
+      } else if (changeX < 0 && changeY < 0) {
+        newX -= Math.cos(rad) * speed;
+        newY -= Math.sin(rad) * speed;
+      } else if (changeX >= 0 && changeY < 0) {
+        newX += Math.cos(rad) * speed;
+        newY -= Math.sin(rad) * speed;
+      }
+      boid.x = newX;
+      boid.y = newY;
+    });
+    this.setState({ boids });
+  }
+
+  handleMouseDown({ pageX: x, pageY: y }) {
+    this.mouseIsDown = true;
+  }
+
+  handleMouseUp({ pageX: x, pageY: y }) {
+    this.mouseIsDown = false;
+  }
+
+  addBoid(x, y) {
+    const key = uid();
+    const deg = Math.random() * 360;
+    const boids = [...this.state.boids, { key, x, y, deg }];
+    this.setState({ boids });
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.boids.map((boid) => (
+          <Boid
+            key={boid.key}
+            id={boid.key}
+            x={boid.x}
+            y={boid.y}
+            deg={boid.deg}
+          />
+        ))}
+      </>
+    );
+  }
 }
+
+export default Boids;
